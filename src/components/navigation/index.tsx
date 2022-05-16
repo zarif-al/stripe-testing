83f6be63801d043e61bd2a708996f0cb02b9ad03
zarif-al <zarif_al96@outlook.com>
2022-05-16 12:01:45 +0600

    style update

--- src/components/navigation/index.tsx
+++ src/components/navigation/index.tsx
@@ -1,34 +1,39 @@
 import React, { useContext } from "react";
 import { AuthContext } from "src/contexts/auth";
+import { Button } from "antd";
 
 const Navigation = () => {
-	const { signOut, firebaseUser } = useContext(AuthContext);
+	const { signOut, firebaseUser, dbUser } = useContext(AuthContext);
 
-	return (
-		<div
-			style={{
-				position: "sticky",
-				top: 0,
-				display: "flex",
-				justifyContent: "flex-end",
-				alignItems: "center",
-				height: "3rem",
-				backgroundColor: "#DCDCDC",
-				borderBottom: "2px solid black",
-			}}
-		>
-			{firebaseUser && (
-				<button
-					style={{ height: "2rem" }}
-					onClick={() => {
-						signOut();
-					}}
-				>
-					Sign Out
-				</button>
-			)}
-		</div>
-	);
+	if (firebaseUser && dbUser) {
+		return (
+			<div
+				style={{
+					position: "sticky",
+					top: 0,
+					display: "flex",
+					justifyContent: "flex-end",
+					alignItems: "center",
+					height: "3rem",
+					backgroundColor: "#DCDCDC",
+					borderBottom: "2px solid black",
+				}}
+			>
+				<div style={{ marginRight: "15px" }}>
+					<Button
+						type="primary"
+						onClick={() => {
+							signOut();
+						}}
+					>
+						Sign Out
+					</Button>
+				</div>
+			</div>
+		);
+	} else {
+		return <></>;
+	}
 };
 
 export default Navigation;
