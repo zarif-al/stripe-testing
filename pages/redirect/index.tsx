dabbd5b17491c260953418695cfd3dec156e12fd
zarif-al <zarif_al96@outlook.com>
2022-05-21 16:19:26 +0600

    todo comment

--- pages/redirect/index.tsx
+++ pages/redirect/index.tsx
@@ -76,6 +76,7 @@ function Redirect() {
 		return (...args: [user: IUser, price_id: string]) => {
 			clearTimeout(timer);
 			timer = setTimeout(() => {
+				// TODO : FIX THIS
 				//@ts-ignore
 				func.apply(this as any, args as [user: IUser, price_id: string]);
 			}, timeout);
